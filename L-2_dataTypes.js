98cc88542ce1af576985a8b46d08b2b7ea8aa4bd
rameshhh <rameshjha55061@gmail.com>
2023-11-15 18:09:36 +0530

    dataTypes

---
+++ L-2_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello"); // this code is run in browser not in nodejs
+
+let name = "Ramesh";
+let age = 18;
+let isLoggedIn;
+
+/*
+// Types of dataTypes
+
+1 - number => 2 to power 53
+2 - bigint
+3 - string => ""
+4 - boolean => true/false
+5 - null => standalone value // if temperature value is not coming form api , it will show 0 that is also a value , for that time we need "null" because it will NOT show any value
+6 - undefined => value is not defined yet
+7 - symbole => for finding uniqueness 
+
+8 - object
+*/
+console.log(typeof undefined);  // will give undefined
+console.log(typeof null);  // will give object
