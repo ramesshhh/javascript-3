b65b8aefc80e1dfda0812e3f7da8f9ef2fc25642
rameshhh <rameshjha55061@gmail.com>
2023-11-17 01:49:52 +0530

    Data Type Summery

---
+++ L-5_dataTypeSummery.js
@@ -0,0 +1,10 @@
+// Primitive
+
+// 7 Types: String , Number , Boolean , Null , Undefined , Symbole , BigInt
+
+
+// Reference type / Non Primitive
+
+// 3 type: Arrays , Object , Function
+
+// JS is a dynamic or static type language: JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
